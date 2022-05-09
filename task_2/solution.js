52f34060f3fb30e28af118fe93df1a79e33301ad
Starodubtseva Tatiana <tatulik24951@gmail.com>
2022-05-09 16:58:25 +0300

    2.3 check

--- task_2/solution.js
+++ task_2/solution.js
@@ -26,7 +26,7 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     let discountSum
-    
+
     if (productsSum >= discountMinSum) {
         discountSum = discountPart / 100 * productsSum;
     } else {
@@ -42,18 +42,18 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
-    // создайте переменную totalSum
+    let totalSum
 
-    // присвойте totalSum значение productsSum
-    // уменьшите totalSum на discountSum
+    totalSum = productsSum
+    totalSum = totalSum - discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
-    // прибавьте к totalSum значение shippingSum
+    totalSum = totalSum + shippingSum
 
-    // создайте переменную freeShipping
-    // запишите без использования if или любых других условий:
-    // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    let freeShipping
+
+    (shippingSum == 0) ? freeShipping = true : freeShipping = false;
 
     // Конец решения задачи №2.3.
 
